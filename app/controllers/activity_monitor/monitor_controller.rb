c39673db8f6898af29f16b91d2a5fc238ede3875
dkukhl <dkukhl@yandex.ru>
2017-02-07 13:45:13 +0300

    added table data

--- app/controllers/activity_monitor/monitor_controller.rb
+++ app/controllers/activity_monitor/monitor_controller.rb
@@ -2,16 +2,25 @@ require_dependency "activity_monitor/application_controller"
 require_dependency 'groupdate'
 require_dependency 'chartkick'
 
-
 module ActivityMonitor
   class MonitorController < ApplicationController
     def index
       @keys = PublicActivity::Activity.uniq.pluck(:key)
+      @data = PublicActivity::Activity.group_by_day(:created_at).count
+      @activities =  scope_activities.limit(20)
     end
 
     def show
       @key = CGI.unescape(params[:key])
       @data = PublicActivity::Activity.where(key: @key).group_by_day(:created_at).count
+      @activities = scope_activities.where(key: @key).limit(20)
+    end
+
+
+    private
+
+    def scope_activities
+      PublicActivity::Activity.order(created_at: :desc).includes(:owner)
     end
   end
 end
--- app/jobs/activity_monitor/save_activity_job.rb
+++ app/jobs/activity_monitor/save_activity_job.rb
@@ -4,7 +4,7 @@ module ActivityMonitor
 
     def perform(user_id, controller, action)
       user = ActivityMonitor.user_class.constantize.find(user_id)
-      user.create_activity key: "user.#{controller}-#{action}"
+      user.create_activity key: "user.#{controller}-#{action}", owner: user
     end
   end
 end
